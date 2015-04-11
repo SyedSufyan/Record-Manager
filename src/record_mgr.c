3cc77a7a26a0f11313d869f9b6b30c6612ba30e8
SyedSufyan <ssher@hawk.iit.edu>
2015-04-10 19:44:14 -0500

    modified record

--- src/record_mgr.c
+++ src/record_mgr.c
@@ -9,16 +9,6 @@
 #include "dberror.h"
 #include "record_mgr.h"
 
-//structure to store scaned data
-struct ScanData {
-    Expr *cond;
-    int currRecord;
-    int numRecords;
-    int no_blocks;
-    int currentblock;
-    int parsedRecords;
-} ScanData;
-
 // table and manager
 extern RC initRecordManager (void *mgmtData);
 extern RC shutdownRecordManager ();
@@ -37,15 +27,48 @@ extern RC getRecord (RM_TableData *rel, RID id, Record *record);
 // scans
 RC startScan (RM_TableData *rel, RM_ScanHandle *scan, Expr *cond)
 {
+//need to add error check	
+//Copying all the attributes from the table to the Scan Table variable
 
+// Allocate memory for RM_tableData in scan
+    scan->rel = (RM_TableData *) malloc(sizeof(RM_TableData));
+    // Update table name in the scan's RM_tableData
+    scan->rel->name = (char *) malloc (sizeof(char) * strlen(rel->name));
+    strcpy(scan->rel->name, rel->name);
+
+    // Update schema in scan's RM_TableData
+    scan->rel->schema = createSchema(rel->schema->numAttr, rel->schema->attrNames, rel->schema->dataTypes, rel->schema->typeLength, rel->schema->keySize, rel->schema->keyAttrs)
+
+    // Allocate memory for RM_tableMgmtData
+    scan->rel->mgmtData = (RM_tableMgmtData *) malloc(sizeof(RM_tableMgmtData));
+    scan->rel->mgmtData->bm = rel->mgmtData->bm;    // Bufferpool
+    scan->rel->mgmtData->numRecords = rel->mgmtData->numRecords;    // Assign numRecords
+    scan->rel->mgmtData->recLength = rel->mgmtData->recLength;    // Assign recLength
+    scan->rel->mgmtData->totalTableBlocks = rel->mgmtData->totalTableBlocks;    // # table blocks.
+
+    // Allocate memory for scan related management data
+    scan->mgmtData = (RM_scanMgmtData *) malloc(sizeof(RM_scanMgmtData));
+    scan->mgmtData->currentRID = (RID *) malloc(sizeof(RID));    // Allocate mem for RID
+//    scan->mgmtData->expr = (Expr *) malloc(sizeof(Expr));       // Mem for Expression.
+//    memcpy(scan->mgmtData->expr, cond, sizeof(Expr));
+
+    scan->mgmtData->expr = cond;
+
+    // Initialize currentRID for scan
+    //scan->mgmtData->currentRID->page = 1;
+    //scan->mgmtData->currentRID->slot = -1;
+
+    return RC_OK;
 }
 extern RC next (RM_ScanHandle *scan, Record *record);
 extern RC closeScan (RM_ScanHandle *scan);
 
 // dealing with schemas
 extern int getRecordSize (Schema *schema);
+//simple Create Schema
 Schema *createSchema (int numAttr, char **attrNames, DataType *dataTypes, int *typeLength, int keySize, int *keys)
-{
+{//need to add error check ?
+
 // Allocate memory for schema
     Schema *newSchema = (Schema *) malloc(sizeof(Schema));
     newSchema->numAttr = numAttr; // # of attributes in schema
